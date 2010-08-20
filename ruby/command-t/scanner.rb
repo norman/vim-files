6f2af022e01d8fb17a8d2d319a3526765d9f2bae
Norman Clarke <norman@njclarke.com>
2010-08-20 12:04:27 -0300

    Updated to latest Command-T.

--- ruby/command-t/scanner.rb
+++ ruby/command-t/scanner.rb
@@ -21,6 +21,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+require 'command-t/vim'
+
 module CommandT
   # Reads the current directory recursively for the paths to all regular files.
   class Scanner
@@ -62,8 +64,8 @@ module CommandT
     def path_excluded? path
       # first strip common prefix (@path) from path to match VIM's behavior
       path = path[(@prefix_len + 1)..-1]
-      path = Vim.escape_for_single_quotes path
-      VIM.evaluate("empty(expand(fnameescape('#{path}')))").to_i == 1
+      path = VIM::escape_for_single_quotes path
+      ::VIM::evaluate("empty(expand(fnameescape('#{path}')))").to_i == 1
     end
 
     def add_paths_for_directory dir, accumulator
